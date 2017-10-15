015fd49041c667d01e413550160952ea7fe1228e
kakaka19 <kkatum@gmail.com>
2017-10-16 01:56:47 +0900

    node-aplayのSoundオブジェクトをラップするクラスを作成

--- test/sound.js
+++ test/sound.js
@@ -1,2 +1,18 @@
-var Sound = require('node-aplay');
-new Sound('sample.wav').play();
+const Sound = require('node-aplay');
+
+var SoundPlayer = function(){
+  // 初期化時にデフォルトの音源を読み込み
+  this.musicFilePath = './sample.wav'
+  this.soundObj = new Sound();
+};
+
+SoundPlayer.prototype = {
+  //個別の音源のファイルパスをセットする
+  setSound: fucntion(filepath){
+    this.musicFilePath = filepath;
+  },
+  //音源を再生する
+  play; fucntion(){
+    this.soundObj.play(this.musicFilePath);
+  }
+};
