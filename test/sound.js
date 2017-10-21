d55bebd6890c1469868bbecd097840bdb1925043
kakaka19 <kkatum@gmail.com>
2017-10-21 13:34:18 +0900

    デファルト音源のパスを個別に変数に格納

--- test/sound.js
+++ test/sound.js
@@ -2,18 +2,23 @@ const Sound = require('node-aplay');
 
 function SoundPlayer(){
   // 初期化時にデフォルトの音源を読み込み
-  this.musicFilePath = './sample.wav'
+  this.defaultMusic = './sample.wav';
+  this.selectMusic = null;
   this.soundObj = new Sound();
 };
 
 SoundPlayer.prototype = {
   //個別の音源のファイルパスをセットする
   setSound: function(filepath){
-    this.musicFilePath = filepath;
+    this.selectMusic = filepath;
   },
   //音源を再生する
   play: function(){
-    this.soundObj.play(this.musicFilePath);
+    if(this.selectMusic == null){
+	    this.soundObj.play(this.defaultMusic);
+    	    return;
+    }
+    this.soundObj.play(this.selectMusic);
   }
 };
 
