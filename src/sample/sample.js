95d828b7f0cac7c624fdf9954a4429bd6434989a
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2017-12-17 00:10:15 +0900

    UTテスト環境構築。css-modules対応。webpackの設定をpolyfillが読み込めるように修正。

---
+++ src/sample/sample.js
@@ -0,0 +1,7 @@
+export const sample1 = () => ({
+  type: 'sample1',
+});
+
+export const sample2 = () => ({
+  type: 'sample2',
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,35 @@
 const path = require('path');
 
 const config = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'public/js'),
     filename: 'bundle.js',
   },
+  devtool: 'cheap-source-map',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.spec\.js$/,
+        loader: 'webpack-espower-loader',
       },
     ],
   },
