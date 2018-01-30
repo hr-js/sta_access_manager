e7082b36e6d5ca01595449559208ce2241c68d81
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-01-31 02:12:05 +0900

    カード読み取り時に、音がなるように機能を実装

--- modules/setUpSocketIo.js
+++ modules/setUpSocketIo.js
@@ -8,12 +8,19 @@ var _socket = require('socket.io');
 
 var _socket2 = _interopRequireDefault(_socket);
 
+var _nodeNfcpyId = require('node-nfcpy-id');
+
+var _nodeNfcpyId2 = _interopRequireDefault(_nodeNfcpyId);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-console.log(_socket2.default);
+// import path from 'path';
 
-var socketIo = require('socket.io');
-console.log(socketIo);
+var Sound = require('aplay');
+
+// const music = new Sound();
+
+var nfc = new _nodeNfcpyId2.default().start();
 
 var setUpSocketIo = function setUpSocketIo(server) {
   var io = (0, _socket2.default)(server);
@@ -25,12 +32,26 @@ var setUpSocketIo = function setUpSocketIo(server) {
       console.log('接続が切れました。');
     });
 
-    socket.on('chat message', function (msg) {
-      console.log('\u53D7\u4FE1\u3057\u305F\u30E1\u30C3\u30BB\u30FC\u30B8:' + msg);
-      io.emit('push', msg);
+    // touch scan
+    nfc.on('touchstart', function (card) {
+      console.log('Card ID: ' + card.id);
+      console.log('Card Type: ' + card.type);
+
+      // music.play(path.resolve(__dirname, './assets/music/sample.wav'));
+
+      new Sound().play('./assets/music/hoge.mp3');
+      socket.emit('scan', card.id);
+    });
+    // touch end
+    nfc.on('touchend', function () {
+      console.log('Card was away.');
+    });
+    nfc.on('error', function (err) {
+      // standard error output (color is red)
+      console.error('\x1B[31m', err, '\x1B[0m');
     });
   });
+  // socket.emit('scan', data);
 };
-
 exports.default = setUpSocketIo;
 //# sourceMappingURL=setUpSocketIo.js.map
--- src/server/modules/setUpSocketIo.js
+++ src/server/modules/setUpSocketIo.js
@@ -1,4 +1,12 @@
 import Server from 'socket.io';
+import NfcpyId from 'node-nfcpy-id';
+// import path from 'path';
+
+const Sound = require('aplay');
+
+// const music = new Sound();
+
+const nfc = new NfcpyId().start();
 
 const setUpSocketIo = (server) => {
   const io = Server(server);
@@ -9,10 +17,26 @@ const setUpSocketIo = (server) => {
     socket.on('disconnect', () => {
       console.log('接続が切れました。');
     });
-  });
 
-  // TODO: スキャンカードイベントを登録
+    // touch scan
+    nfc.on('touchstart', (card) => {
+      console.log(`Card ID: ${card.id}`);
+      console.log(`Card Type: ${card.type}`);
+
+      // music.play(path.resolve(__dirname, './assets/music/sample.wav'));
+
+      new Sound().play('./assets/music/hoge.mp3'); // assets/musicにあるmp3ファイルを指定します。
+      socket.emit('scan', card.id);
+    });
+    // touch end
+    nfc.on('touchend', () => {
+      console.log('Card was away.');
+    });
+    nfc.on('error', (err) => {
+      // standard error output (color is red)
+      console.error('\u001b[31m', err, '\u001b[0m');
+    });
+  });
   // socket.emit('scan', data);
 };
-
 export default setUpSocketIo;
