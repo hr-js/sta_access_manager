b88eccc2fc72f87df60be5831547255625300629
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-02-10 00:29:49 +0900

    開発時にでエラーが出ないようICカードスキャンの処理をコメントアウト化。src/server/modules/setUpSocketIo.js内

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,10 @@
+// @flow
 import io from 'socket.io-client';
 
 const socket = io('localhost:3000');
 
 socket.on('connect', () => {
-  socket.on('scan', (data) => {
+  socket.on('scan', (data: string) => {
     // スキャンしたデータはここに表示される
     console.log(`message: ${data}`);
   });
--- src/server/modules/setUpSocketIo.js
+++ src/server/modules/setUpSocketIo.js
@@ -1,12 +1,10 @@
 import Server from 'socket.io';
-import NfcpyId from 'node-nfcpy-id';
-// import path from 'path';
+// エラー回避のためコメントアウトしています。
+// import NfcpyId from 'node-nfcpy-id';
 
-const Sound = require('aplay');
+// const Sound = require('aplay');
 
-// const music = new Sound();
-
-const nfc = new NfcpyId().start();
+// const nfc = new NfcpyId().start();
 
 const setUpSocketIo = (server) => {
   const io = Server(server);
@@ -19,23 +17,20 @@ const setUpSocketIo = (server) => {
     });
 
     // touch scan
-    nfc.on('touchstart', (card) => {
-      console.log(`Card ID: ${card.id}`);
-      console.log(`Card Type: ${card.type}`);
-
-      // music.play(path.resolve(__dirname, './assets/music/sample.wav'));
-
-      new Sound().play('./assets/music/hoge.mp3'); // assets/musicにあるmp3ファイルを指定します。
-      socket.emit('scan', card.id);
-    });
-    // touch end
-    nfc.on('touchend', () => {
-      console.log('Card was away.');
-    });
-    nfc.on('error', (err) => {
-      // standard error output (color is red)
-      console.error('\u001b[31m', err, '\u001b[0m');
-    });
+    // nfc.on('touchstart', (card) => {
+    //   console.log(`Card ID: ${card.id}`);
+    //   console.log(`Card Type: ${card.type}`);
+    //   new Sound().play('./assets/music/hoge.mp3'); // assets/musicにあるmp3ファイルを指定します。
+    //   socket.emit('scan', card.id);
+    // });
+    // // touch end
+    // nfc.on('touchend', () => {
+    //   console.log('Card was away.');
+    // });
+    // nfc.on('error', (err) => {
+    //   // standard error output (color is red)
+    //   console.error('\u001b[31m', err, '\u001b[0m');
+    // });
   });
   // socket.emit('scan', data);
 };
