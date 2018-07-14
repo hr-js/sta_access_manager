6d59267ad0c83eac9e5908b1e05af53ca6e5628b
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-07-14 16:28:08 +0900

    [change] 背景色を指定できるように変更

--- test/client/components/MainFlame.spec.jsx
+++ test/client/components/MainFlame.spec.jsx
@@ -50,7 +50,96 @@ describe("MainFlame.jsxのテスト", () => {
           <TestComponent />
         </MainFlame>
       );
+      expect(mainFlame.children().contains(TestComponent)).toBeTruthy();
       expect(mainFlame.children().text()).toBe("test component");
+    });
+
+    describe("type: workを指定した時", () => {
+      it("cssクラス: workをもつ", () => {
+        const mainFlame = shallow(
+          <MainFlame type="work">
+            <TestComponent />
+          </MainFlame>
+        );
+        expect(mainFlame.find("main").hasClass("work")).toBeTruthy();
+      });
+    });
+
+    describe("type: studyを指定した時", () => {
+      it("cssクラス: studyをもつ", () => {
+        const mainFlame = shallow(
+          <MainFlame type="study">
+            <TestComponent />
+          </MainFlame>
+        );
+        expect(mainFlame.find("main").hasClass("study")).toBeTruthy();
+      });
+    });
+
+    describe("type: meetUpを指定した時", () => {
+      it("cssクラス: meetUpをもつ", () => {
+        const mainFlame = shallow(
+          <MainFlame type="meetUp">
+            <TestComponent />
+          </MainFlame>
+        );
+        expect(mainFlame.find("main").hasClass("meetUp")).toBeTruthy();
+      });
+    });
+
+    describe("type: circleを指定した時", () => {
+      it("cssクラス: circleをもつ", () => {
+        const mainFlame = shallow(
+          <MainFlame type="circle">
+            <TestComponent />
+          </MainFlame>
+        );
+        expect(mainFlame.find("main").hasClass("circle")).toBeTruthy();
+      });
+    });
+
+    describe("type: exitを指定した時", () => {
+      it("cssクラス: exitをもつ", () => {
+        const mainFlame = shallow(
+          <MainFlame type="exit">
+            <TestComponent />
+          </MainFlame>
+        );
+        expect(mainFlame.find("main").hasClass("exit")).toBeTruthy();
+      });
+    });
+
+    describe("type: errorを指定した時", () => {
+      it("cssクラス: errorをもつ", () => {
+        const mainFlame = shallow(
+          <MainFlame type="error">
+            <TestComponent />
+          </MainFlame>
+        );
+        expect(mainFlame.find("main").hasClass("error")).toBeTruthy();
+      });
+    });
+
+    describe("typeに上記以外を指定した時", () => {
+      it("cssクラス: standardをもつ", () => {
+        const mainFlame = shallow(
+          <MainFlame type="standard">
+            <TestComponent />
+          </MainFlame>
+        );
+        expect(mainFlame.find("main").hasClass("standard")).toBeTruthy();
+      });
+    });
+
+    describe("typeを指定しない時", () => {
+      it("cssクラス: standardをもつ", () => {
+        const mainFlame = shallow(
+          <MainFlame type="standard">
+            <TestComponent />
+          </MainFlame>
+        );
+        expect(mainFlame.find("main").hasClass("standard")).toBeTruthy();
+      });
     });
   });
 });
