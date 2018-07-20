7c05412009b5c11c3f5da4e250d0833f99b61a89
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-07-20 20:35:30 +0900

    [change] Participant/ListBodyコンポーネントのテスト方法を変更

--- test/client/components/Participant.ListBody.spec.jsx
+++ test/client/components/Participant.ListBody.spec.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 import ListBody from "@components/Participant/ListBody";
+import Purpose from "@components/Participant/Purpose";
+import Status from "@components/Participant/Status";
 import { shortParticipantData, longParticipantData } from "./testData";
 
 describe("Participant.ListBody.jsxのテスト", () => {
   describe("スナップショット", () => {
-    it("正しいレンタリング", () => {
+    it("正しいレンダリング", () => {
       const tree = renderer
         .create(<ListBody listData={shortParticipantData} />)
         .toJSON();
@@ -15,49 +17,42 @@ describe("Participant.ListBody.jsxのテスト", () => {
   });
 
   describe("コンポーネントのテスト", () => {
-    it("Purposeコンポーネントのpurposeに、値を渡している", () => {
-      const purposeComponents = shallow(
-        <ListBody listData={shortParticipantData} />
-      ).find("Purpose");
+    const listBody = shallow(<ListBody listData={shortParticipantData} />);
+    const listItems = listBody.find("li");
 
-      shortParticipantData.forEach((data, index) => {
-        expect(purposeComponents.at(index).prop("purpose")).toBe(data.purpose);
-      });
+    it("Purposeコンポーネントのpurposeに、値を渡している", () => {
+      listItems.forEach((node, index) =>
+        expect(
+          node.contains(
+            <Purpose purpose={shortParticipantData[index].purpose} />
+          )
+        )
+      );
     });
 
     it("StatusコンポーネントのisEntryに、値を渡している", () => {
-      const statusComponents = shallow(
-        <ListBody listData={shortParticipantData} />
-      ).find("Status");
-
-      statusComponents.forEach((node, index) => {
-        if (shortParticipantData[index].isEntry) {
-          expect(node.prop("isEntry")).toBeTruthy();
-        } else {
-          expect(node.prop("isEntry")).toBeFalsy();
-        }
-      });
+      listItems.forEach((node, index) =>
+        expect(
+          node.contains(
+            <Status isEntry={shortParticipantData[index].isEntry} />
+          )
+        )
+      );
     });
 
-    describe("listDataにshortParticipantDataを指定した時", () => {
-      it("listItemの要素全てにcssクラス: buttomをもつ", () => {
-        const listItems = shallow(
-          <ListBody listData={shortParticipantData} />
-        ).find("li");
-
-        listItems.forEach(node => {
-          expect(node.hasClass("buttom")).toBeTruthy();
-        });
+    it("listItemの要素全てにcssクラス: buttomをもつ", () => {
+      listItems.forEach(node => {
+        expect(node.hasClass("buttom")).toBeTruthy();
       });
     });
 
     describe("listDataにlongParticipantDataを指定した時", () => {
       it("最後の要素はcssクラス: buttomをもたない", () => {
-        const listItems = shallow(
+        const longListItems = shallow(
           <ListBody listData={longParticipantData} />
         ).find("li");
-        const { length } = listItems;
-        listItems.forEach((node, index) => {
+        const { length } = longListItems;
+        longListItems.forEach((node, index) => {
           if (index === length - 1) {
             expect(node.hasClass("buttom")).toBeFalsy();
           } else {
