c90bddf50e7a5ba0017460ca02703150ede7921c
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-08-05 14:41:56 +0900

    [change] Registerのcurrent値を追加

--- src/client/components/Register/Header.jsx
+++ src/client/components/Register/Header.jsx
@@ -18,7 +18,7 @@ function getClassName(current: string): ClassNameType {
   if (current === "input") {
     return { input: active, scan: disabled, completion: disabled };
   }
-  if (current === "scan") {
+  if (current === "scan" || current === "registrationIn") {
     return { scan: active, completion: disabled };
   }
   if (current === "failed") {
