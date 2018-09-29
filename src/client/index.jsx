ba673bb4aed4129408ae21f6f46b7f487cd89b69
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-09-29 15:29:55 +0900

    [change] index.jsxを変更

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,7 @@
 // @flow
-import io from "socket.io-client";
 import * as React from "react";
 import ReactDOM from "react-dom";
-import App from "@components/App";
+import App from "@containers/App";
 
 const root = document.getElementById("root");
 if (root === null) {
@@ -10,11 +9,3 @@ if (root === null) {
 }
 
 ReactDOM.render(<App />, root);
-
-const socket: Socket = io("localhost:3000");
-
-socket.on("connect", () => {
-  socket.on("scan", (data: string) => {
-    console.log(`message: ${data}`);
-  });
-});
