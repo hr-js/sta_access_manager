79b3b1160ea34898423adcbf0af91e2ca1adf146
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-09-29 15:19:44 +0900

    [change] Menuコンポーネントを変更

--- src/client/components/Menu/index.jsx
+++ src/client/components/Menu/index.jsx
@@ -1,31 +1,43 @@
 // @flow
+import type { MenuProps, MenuButton } from "@types";
+
 import * as React from "react";
+import {
+  TOP,
+  TOP_PATH,
+  REGISTER,
+  REGISTER_PATH,
+  PARTICIPANT,
+  PARTICIPANT_PATH
+} from "@constants";
+
 import styles from "./style.css";
 
-type PropType = {
-  current: string,
-  onButtonClick: (current: string) => void
-};
+const menuList: Array<MenuButton> = [
+  { text: "入退室処理", name: TOP, path: TOP_PATH },
+  { text: "ユーザー登録", name: REGISTER, path: REGISTER_PATH },
+  { text: "利用者一覧", name: PARTICIPANT, path: PARTICIPANT_PATH }
+];
 
-const menuList: Array<string> = ["入退室処理", "ユーザ登録", "利用者一覧"];
-
-function Menu(props: PropType): React.Node {
-  const { current, onButtonClick } = props;
+function Menu(props: MenuProps): React.Node {
+  const { selectedMenu, onButtonClick } = props;
   const { menu, list, button, selected } = styles;
 
   const listItems: Array<React.Node> = menuList.map(
-    (item: string): React.Node => {
+    (item: MenuButton): React.Node => {
+      const { name, path, text } = item;
       const className: string =
-        item === current ? `${button} ${selected}` : button;
+        name === selectedMenu ? `${button} ${selected}` : button;
+
       return (
-        <li key={item}>
+        <li key={name}>
           <button
             className={className}
             onClick={() => {
-              onButtonClick(item);
+              onButtonClick(path);
             }}
           >
-            {item}
+            {text}
           </button>
         </li>
       );
