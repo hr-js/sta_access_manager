f3608d3b9d3c257e9882e18a6c40f128097209cc
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-09-29 15:21:17 +0900

    [change] Purposeコンポーネントを変更

--- src/client/components/Purpose/index.jsx
+++ src/client/components/Purpose/index.jsx
@@ -1,39 +1,39 @@
 // @flow
+import type { PurposeProps } from "@types";
+
 import * as React from "react";
 import styles from "./style.css";
 
-type PurposeType = {
-  onMeetUpButtonClick: (e: SyntheticEvent<HTMLButtonElement>) => {},
-  onWorkButtonClick: (e: SyntheticEvent<HTMLButtonElement>) => {},
-  onStudyButtonClick: (e: SyntheticEvent<HTMLButtonElement>) => {},
-  onCircleButtonClick: (e: SyntheticEvent<HTMLButtonElement>) => {}
-};
+class Purpose extends React.Component<PurposeProps> {
+  componentDidMount() {
+    this.props.checkId();
+  }
 
-function Purpose(props: PurposeType): React.Node {
-  const {
-    onMeetUpButtonClick,
-    onWorkButtonClick,
-    onStudyButtonClick,
-    onCircleButtonClick
-  } = props;
-  const { container, meetUp, work, study, circle, message } = styles;
+  componentWillUnmount() {
+    this.props.unmount();
+  }
 
-  return (
-    <div className={container}>
-      <button className={meetUp} onClick={onMeetUpButtonClick}>
-        勉強会
-      </button>
-      <button className={work} onClick={onWorkButtonClick}>
-        仕事
-      </button>
-      <button className={study} onClick={onStudyButtonClick}>
-        自習
-      </button>
-      <button className={circle} onClick={onCircleButtonClick}>
-        サークル
-      </button>
-      <div className={message}>目的を選択して下さい</div>
-    </div>
-  );
+  render() {
+    const { onPurposeButtonClick } = this.props;
+    const { container, meetup, work, study, circle, message } = styles;
+
+    return (
+      <div className={container}>
+        <button id="meetup" className={meetup} onClick={onPurposeButtonClick}>
+          勉強会
+        </button>
+        <button id="work" className={work} onClick={onPurposeButtonClick}>
+          仕事
+        </button>
+        <button id="study" className={study} onClick={onPurposeButtonClick}>
+          自習
+        </button>
+        <button id="circle" className={circle} onClick={onPurposeButtonClick}>
+          サークル
+        </button>
+        <div className={message}>目的を選択して下さい</div>
+      </div>
+    );
+  }
 }
 export default Purpose;
