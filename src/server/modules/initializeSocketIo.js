6c90a73014d1431bd56ba34f88dfe2f2d44b545b
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-10-06 17:05:42 +0900

    [change] スクリプトでスキャン音を出せるように変更

--- src/server/modules/initializeSocketIo.js
+++ src/server/modules/initializeSocketIo.js
@@ -1,18 +1,20 @@
 // @flow
 import SocketIo from "socket.io";
 import NfcpyId from "node-nfcpy-id";
-import Sound from "aplay";
+
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const nfc = new NfcpyId().start();
 
 function initializeSocketIo(server: Server) {
   const io: SocketIo.Server = SocketIo(server);
   io.on("connection", (socket: SocketIo.Socket) => {
-    socket.on("sound", id => {
+    socket.on("sound", async id => {
       // idで音を変更できる
       switch (id) {
         default:
-          new Sound().play("./assets/music/xxx.mp3");
+          await exec("mpg321 ./assets/music/xxx.mp3");
           break;
       }
     });
